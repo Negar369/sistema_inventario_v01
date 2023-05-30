f2a8dc50e4163cb71fb013f6c57b0cacade3ae87
Negar369 <134983310+Negar369@users.noreply.github.com>
2023-05-30 08:45:34 -0500

    sumid_android

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Mi proyecto flutter desde visual'),
+      home: const MyHomePage(title: 'Mi proyecto flutter desde android'),
     );
   }
 }
